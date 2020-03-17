b5a79aa03859bee439520c3cbb61ff5e07b9550c
jipey <jpcarlens@outlook.fr>
2020-03-17 04:05:18 +0100

    Restructuration du code

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from .views import app, socket_io
 '''
 
 from flask import Flask
+from flask_cors import CORS
 from flask_socketio import SocketIO
 
 flaskapp = Flask(__name__, instance_relative_config=True)
@@ -12,6 +13,7 @@ socket_io = SocketIO()
 def create_app(debug=False):
     flaskapp.config.from_object('config')
     flaskapp.debug = debug
+    CORS(flaskapp, resources={r'/*': {'origins': '*'}})
 
     import app.main.routes
     import app.main.events
